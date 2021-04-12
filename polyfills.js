c2b9fd4184dd9533367e014bfea5162f35825f34
simon <simon.p5207@gmail.com>
2021-04-12 14:59:49 +0200

    site sans back

--- polyfills.js
+++ polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Bureau\site\dashproject\dashfront\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! D:\Bureau\site\dashproject - Copie\dashfront\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
